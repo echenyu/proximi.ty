1b9f7dca3cbad8241cbd4c43a6ba47ee78ddd902
Eric Yu <echenyu@umich.edu>
2017-04-18 16:01:13 -0400

    ey - added tableview controller and table view cell to project

---
+++ Proximi.ty/Proximi.ty/ContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactTableViewCell.swift
+//  Proximi.ty
+//
+//  Created by Eric Yu on 4/18/17.
+//  Copyright © 2017 ericYu. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Proximi.ty/Proximi.ty/ViewController.swift
+++ Proximi.ty/Proximi.ty/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let x = "haha eric"
+        var y = 10
+        
+        y = 15
         // Do any additional setup after loading the view, typically from a nib.
     }
 
